118f46b7a916731de061a332de78628ad2543a6f
Banchai Nangpang <pong.np1@gmail.com>
2026-08-10 20:58:28 +0700

    chore: remove debug print logs from SceneDelegate and AppDelegate

--- Example/BNImagePageView/AppDelegate.swift
+++ Example/BNImagePageView/AppDelegate.swift
@@ -19,10 +19,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         }
         switch topController {
         case is BNImagePageGridView, is BNImagePageGridHideShareView, is BNImagePageViewController:
-            print("[BNOrientation] \(type(of: topController!)) → .all")
             return .all
         default:
-            print("[BNOrientation] \(type(of: topController as AnyObject)) → .portrait")
             return .portrait
         }
     }
