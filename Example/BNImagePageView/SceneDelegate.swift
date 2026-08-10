094804f9ed1048a4da16573aa0a582cd4cd94d7c
Banchai Nangpang <pong.np1@gmail.com>
2026-08-10 16:25:18 +0700

    fix: adopt UIScene lifecycle in Example app

---
+++ Example/BNImagePageView/SceneDelegate.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        let window = UIWindow(windowScene: windowScene)
+        let nav = UINavigationController(rootViewController: ViewController())
+        nav.isNavigationBarHidden = true
+        window.rootViewController = nav
+        self.window = window
+        window.makeKeyAndVisible()
+    }
+}
