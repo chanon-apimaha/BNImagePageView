f47c23be48d05e12f56e7520cf3e62101a977052
Banchai Nangpang <pong.np1@gmail.com>
2026-08-10 16:06:05 +0700

    fix: lower SPM minimum iOS to 12 for UIKit compatibility

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 
 let package = Package(
     name: "BNImagePageView",
-    platforms: [.iOS(.v13)],
+    platforms: [.iOS(.v12)],
     products: [
         .library(name: "BNImagePageView", targets: ["BNImagePageView"])
     ],
