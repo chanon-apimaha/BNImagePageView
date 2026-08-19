75c63bf7f4ea5c9a156325031a1ea255c7e34a49
Banchai Nangpang <pong.np1@gmail.com>
2026-08-19 17:01:40 +0700

    fix: force UIDevice.setValue to bypass device orientation lock on all iOS versions

--- BNImagePageView/Classes/BNOrientationHelper.swift
+++ BNImagePageView/Classes/BNOrientationHelper.swift
@@ -6,11 +6,9 @@ public enum BNOrientationHelper {
             guard let scene = UIApplication.shared.connectedScenes
                 .compactMap({ $0 as? UIWindowScene }).first else { return }
             scene.requestGeometryUpdate(.iOS(interfaceOrientations: .all))
-            UIViewController.attemptRotationToDeviceOrientation()
-        } else {
-            UIDevice.current.setValue(UIInterfaceOrientation.unknown.rawValue, forKey: "orientation")
-            UINavigationController.attemptRotationToDeviceOrientation()
         }
+        UIDevice.current.setValue(UIInterfaceOrientation.unknown.rawValue, forKey: "orientation")
+        UIViewController.attemptRotationToDeviceOrientation()
     }
 
     public static func lockPortrait() {
