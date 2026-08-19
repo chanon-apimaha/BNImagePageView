bd0ca2cc2a93c4cbefe1e2e85fa5ce2f0ff33cfa
Banchai Nangpang <pong.np1@gmail.com>
2026-08-19 16:58:56 +0700

    fix: add attemptRotationToDeviceOrientation after unlock on iOS 16+

--- BNImagePageView/Classes/BNOrientationHelper.swift
+++ BNImagePageView/Classes/BNOrientationHelper.swift
@@ -6,6 +6,7 @@ public enum BNOrientationHelper {
             guard let scene = UIApplication.shared.connectedScenes
                 .compactMap({ $0 as? UIWindowScene }).first else { return }
             scene.requestGeometryUpdate(.iOS(interfaceOrientations: .all))
+            UIViewController.attemptRotationToDeviceOrientation()
         } else {
             UIDevice.current.setValue(UIInterfaceOrientation.unknown.rawValue, forKey: "orientation")
             UINavigationController.attemptRotationToDeviceOrientation()
